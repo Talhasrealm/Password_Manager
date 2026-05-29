3f9e29058f939eb5d3b63b7b7a5dc2727a17ec25
Talhasrealm <peertalhatanveer@gmail.com>
2026-05-29 22:08:14 +0300

    Delete db.php

--- db.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');        // leave empty if you didn't set a MySQL password in XAMPP
-define('DB_NAME', 'password_manager');
