ed63377d2d55c6779be1b9ab5347f9da92c88482
Talhasrealm <peertalhatanveer@gmail.com>
2026-05-29 22:10:21 +0300

    Delete PASSWORD_MANAGER directory

--- PASSWORD_MANAGER/classes/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'classes/Database.php';
-
-try {
-    $db = Database::getConnection();
-    echo "✅ Connected to database successfully!";
-} catch (Exception $e) {
-    echo "❌ Error: " . $e->getMessage();
-}
